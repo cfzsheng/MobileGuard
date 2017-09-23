6749e5e1681f681c6816868dc8fd6dc6ea3c2713
cfzsheng <1985277804@qq.com>
2017-09-23 17:21:33 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/HomeActivity.java
@@ -0,0 +1,17 @@
+package cn.edu.gdmec.android.mobileguard;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by pc on 2017/9/23.
+ */
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/enity/VersionEntity.java
@@ -0,0 +1,11 @@
+package cn.edu.gdmec.android.mobileguard.enity;
+
+/**
+ * Created by pc on 2017/9/23.
+ */
+
+public class VersionEntity {
+    public String versionCode;
+    public String description;
+    public String apkurl;
+}
