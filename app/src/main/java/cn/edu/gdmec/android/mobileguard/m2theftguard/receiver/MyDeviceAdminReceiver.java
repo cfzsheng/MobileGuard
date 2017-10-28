16a62b7b7311008dbd54538e7097841dd1f1a7e5
wyhwyhwyh <2836237929@qq.com>
2017-10-29 00:58:39 +0800

    20171029

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/receiver/MyDeviceAdminReceiver.java
@@ -0,0 +1,14 @@
+package cn.edu.gdmec.android.mobileguard.m2theftguard.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MyDeviceAdminReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+    }
+}
